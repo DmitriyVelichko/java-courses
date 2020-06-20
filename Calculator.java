234cca1b104c0704dc02bdf98e2e4d8141195b17
dm.velichko <dm.velichko@gmail.com>
2020-06-20 12:46:29 +0300

    advanced calculator

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,29 @@
 public class Calculator {
 
-    private int result;
+    private int result = 0;
 
+    //Сложение
     public void add(int... params) {
         for(Integer param: params){
             this.result += param;
         }
+    }
+
+    //Вычитание
+    public void sub(int... params) {
+        for(Integer param: params){
+            this.result -= param;
+        }
+    }
+
+    //Умножение
+    public void mul(int... params) {
+        this.result = params[0] * params[1];
+    }
+
+    //Деление
+    public void div(int... params) {
+        this.result = params[0] / params[1];
     }
 
     public int getResult(){
--- InteractRunner.java
+++ InteractRunner.java
@@ -2,24 +2,61 @@ import java.util.Scanner;
 
 public class InteractRunner {
     public static void main(String[] arg) {
-        Scanner reader = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         try {
             Calculator calc = new Calculator();
             String exit = "no";
+            int first = 0;
+            int second = 0;
+            int result = calc.getResult();
+
             while (!exit.equals("yes")) {
-                System.out.println("Input first argument...");
-                String first = reader.next();
+
+                if(result != 0){
+                    System.out.println("First argument " + result);
+                    first = result;
+                } else {
+                    System.out.println("Input first argument...");
+                    first = sc.nextInt();
+                }
+
                 System.out.println("Input Second argument...");
-                String second = reader.next();
-                calc.add(Integer.parseInt(first), Integer.parseInt(second));
-                System.out.println("Result: " + calc.getResult());
-                calc.cleanResult();
+                second = sc.nextInt();
+
+                System.out.println("Change operation? add/sub/mul/div");
+                String operation = sc.next();
+
+                switch (operation) {
+                    case ("add"):
+                        calc.add(first, second);
+                        break;
+                    case ("sub"):
+                        calc.sub(first, second);
+                        break;
+                    case ("mul"):
+                        calc.mul(first, second);
+                        break;
+                    case ("div"):
+                        calc.div(first, second);
+                        break;
+                }
+
+                result = calc.getResult();
+                System.out.println("Result: " + result);
+
+                System.out.println("Save result? yes/no");
+                String save = sc.next();
+                if(save.equals("no")){
+                    calc.cleanResult();
+                    result = 0;
+                }
+
                 System.out.println("Exit? yes/no");
-                exit = reader.next();
+                exit = sc.next();
             }
         } finally {
-            reader.close();
+            sc.close();
         }
     }
 }
